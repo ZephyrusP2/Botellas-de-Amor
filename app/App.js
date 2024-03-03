645a76bc5fd82e05185f1c7f690cfd0c8fe19dd1
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-02 19:09:16 -0500

    Delete quote marks
    Deleted quote marks to check if both workflows can run at the same time

--- app/App.js
+++ app/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     alignItems: "center",
     justifyContent: "center",
   },
