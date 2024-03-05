7b0736543a038a9f15ff54f501b23d8e0866c82f
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-05 08:51:56 -0500

    Add user model
    Added user model with the fields: id, role, name, last_name, birth_date, gender, email and password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "accounts",
 ]
 
+AUTH_USER_MODEL = "accounts.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
