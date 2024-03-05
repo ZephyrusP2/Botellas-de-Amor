c1859ba2fc9e6668d0f71b35e6c09196df936f52
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-05 12:59:37 -0500

    Add classes inside UserView
    Added all the CRUD classes to the UserView to preserve organization

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserView, UserRetrieveUpdateDestroy, UserList
+from .views import UserView
 
 urlpatterns = [
     path('user/signup', UserView.signup, name='user.signup'),
     path('user/login', UserView.login, name='user.login'),
-    path('user/<int:pk>', UserRetrieveUpdateDestroy.as_view(), name='user.detail'),
-    path('user/all', UserList.as_view(), name='user.list'),
+    path('user/<int:pk>', UserView.UserRetrieveUpdateDestroy.as_view(),
+         name='user.detail'),
+    path('user/all', UserView.UserList.as_view(), name='user.list'),
 ]
