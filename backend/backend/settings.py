fc2e29bdf472ae9e029835757df583ac9503b6ce
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-05 08:58:19 -0500

    Add api app base files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "accounts",
+    "api",
 ]
 
 AUTH_USER_MODEL = "accounts.User"
