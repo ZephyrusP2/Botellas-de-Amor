c2d458f799f3f50ec860b18a6007894e17213810
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-12 09:46:33 -0500

    Add admin login
    Added admin login functionality

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -126,3 +126,27 @@ class UserView:
                 except Token.DoesNotExist:
                     token = Token.objects.create(user=user)
                 return JsonResponse({"token": str(token)}, status=200)
+
+    @csrf_exempt
+    def admin_login(request):
+        """
+        Admin login
+        :param request: request
+        :return: JsonResponse
+        """
+        if request.method == "POST":
+            data = JSONParser().parse(request)
+            user = authenticate(
+                request, email=data["email"].lower(), password=data["password"]
+            )
+            if user is None or user.role != "admin":
+                return JsonResponse(
+                    {"error": "could not login. user is not an admin. please check username and/or password"},
+                    status=400,
+                )
+            else:
+                try:
+                    token = Token.objects.get(user=user)
+                except Token.DoesNotExist:
+                    token = Token.objects.create(user=user)
+                return JsonResponse({"token": str(token)}, status=200)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     SERVER_IP,
+    "localhost",
 ]
 
 
