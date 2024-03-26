13e75fe16dd5347d4c36696ed2432ee5a5cadcd2
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-26 14:50:15 -0500

    Add operator login tests

---
+++ backend/accounts/tests/test_operator_login.py
@@ -0,0 +1,58 @@
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from accounts.models import User
+
+# Create your tests here.
+
+
+class OperatorLoginTestCase(APITestCase):
+    def setUp(self):
+        self.operator_user = User.objects.create(
+            name="operator",
+            last_name="operator",
+            birth_date="1990-01-01",
+            gender="Masculino",
+            email="operator@gmail.com",
+            password="operatorpassword",
+            role="operator",
+        )
+        self.operator_user.set_password("operatorpassword")
+        self.operator_user.save()
+        self.token = Token.objects.create(user=self.operator_user)
+        self.url = reverse("operator.login")
+
+        return super().setUp()
+
+    def test_operator_login_success(self):
+        data = {"email": "operator@gmail.com", "password": "operatorpassword"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("token", response.json())
+
+    def test_operator_login_invalid_credentials(self):
+        self.url = reverse("operator.login")
+        data = {"email": "operator@gmail.com", "password": "wrongpassword"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.json())
+
+    def test_operator_login_non_operator_user(self):
+        non_operator_user = User.objects.create(
+            name="user",
+            last_name="user",
+            birth_date="1990-01-01",
+            gender="Masculino",
+            email="user@example.com",
+            password="userpassword",
+            role="user",
+        )
+        non_operator_user.set_password("userpassword")
+        non_operator_user.save()
+        self.token = Token.objects.create(user=non_operator_user)
+        self.url = reverse("operator.login")
+        data = {"email": "user@example.com", "password": "userpassword"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.json())
