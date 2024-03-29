a65116ed1026bd810d7301a321a2659279893b28
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-28 20:08:49 -0500

    Add site update tests
    Added update success, not found, invalid data and unauthorized validation tests

---
+++ backend/disposal/tests/site/test_update.py
@@ -0,0 +1,83 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from accounts.models import User
+from disposal.models import Site
+
+
+class SiteUpdateTestCase(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create(
+            name="admin",
+            last_name="admin",
+            birth_date="1990-01-01",
+            gender="Masculino",
+            email="admin@example.com",
+            password="adminpassword",
+            role="admin",
+        )
+        self.admin_user.set_password("adminpassword")
+        self.admin_user.save()
+        self.site = Site.objects.create(
+            image="path/to/image",
+            opens="10:00:00",
+            closes="17:00:00",
+            name="Universidad EAFIT",
+            address="Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia",
+        )
+
+        return super().setUp()
+
+    def test_site_update_success(self):
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.put(reverse("site.update", args=[self.site.id]), {
+            "image": "path/to/image",
+            "opens": "10:00:00",
+            "closes": "17:00:00",
+            "name": "Universidad EAFIT",
+            "address": "Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia",
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("id", response.json())
+        self.assertIn("name", response.json())
+        self.assertIn("address", response.json())
+        self.assertIn("opens", response.json())
+        self.assertIn("closes", response.json())
+        self.assertIn("image", response.json())
+
+    def test_site_update_unauthorized(self):
+        response = self.client.put(reverse("site.update", args=[self.site.id]), {
+            "image": "path/to/image",
+            "opens": "10:00:00",
+            "closes": "17:00:00",
+            "name": "Universidad EAFIT",
+            "address": "Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia",
+        })
+        self.assertEqual(response.status_code, 403)
+        self.assertIn("detail", response.json())
+
+    def test_site_update_not_found(self):
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.put(reverse("site.update", args=[1000]), {
+            "image": "path/to/image",
+            "opens": "10:00:00",
+            "closes": "17:00:00",
+            "name": "Universidad EAFIT",
+            "address": "Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia",
+        })
+        self.assertEqual(response.status_code, 404)
+        self.assertIn("detail", response.json())
+
+    def test_site_update_invalid_data(self):
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.put(reverse("site.update", args=[self.site.id]), {
+            "image": "path/to/image",
+            "opens": "10:00:00",
+            "closes": "17:00:00",
+            "name": "Universidad EAFIT",
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("address", response.json())
+        self.assertEqual(response.json()["address"], [
+            "This field is required."
+        ])
