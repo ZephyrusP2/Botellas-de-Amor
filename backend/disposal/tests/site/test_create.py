abaa2db632a0aa684f524e905c240724a92a8584
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-28 19:24:21 -0500

    Add site creation tests
    Added create success, invalid data and unauthorized validation tests

---
+++ backend/disposal/tests/site/test_create.py
@@ -0,0 +1,56 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from accounts.models import User
+from disposal.models import Site
+
+
+class SiteCreateTestCase(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create(
+            name="admin",
+            last_name="admin",
+            birth_date="1990-01-01",
+            gender="Masculino",
+            email="admin@example.com",
+            password="adminpassword",
+            role="admin",
+        )
+        self.admin_user.set_password("adminpassword")
+        self.admin_user.save()
+        self.url = reverse("site.create")
+
+        return super().setUp()
+
+    def test_site_create_success(self):
+        self.client.force_authenticate(user=self.admin_user)
+        data = {"image": "path/to/image",
+                "opens": "10:00:00",
+                "closes": "17:00:00",
+                "name": "Universidad EAFIT",
+                "address": "Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("id", response.json())
+
+    def test_site_create_invalid_data(self):
+        self.client.force_authenticate(user=self.admin_user)
+        data = {"image": "path/to/image",
+                "opens": "10:00:00",
+                "closes": "17:00:00",
+                "name": "Universidad EAFIT"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("address", response.json())
+        self.assertEqual(response.json()["address"], [
+                         "This field is required."])
+
+    def test_site_create_unauthorized(self):
+        data = {"image": "path/to/image",
+                "opens": "10:00:00",
+                "closes": "17:00:00",
+                "name": "Universidad EAFIT",
+                "address": "Carrera 49, Cl. 7 Sur #50, Medellín, Antioquia"}
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, 403)
+        self.assertIn("detail", response.json())
