bd4556d75622e0657daa4209318e7a369e8ce01b
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-03-29 20:31:14 -0500

    Refactor user URLs and add tests for user data

--- backend/accounts/tests/test_user_data.py
+++ backend/accounts/tests/test_user_data.py
@@ -4,8 +4,10 @@ from rest_framework.test import APITestCase
 
 from accounts.models import User
 
+# Create your tests here.
 
-class CarbonFootprintTestCase(APITestCase):
+
+class UserDataTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(
             name="user",
@@ -15,32 +17,33 @@ class CarbonFootprintTestCase(APITestCase):
             gender="Masculino",
             email="user@gmail.com",
             password="userpassword",
-            role="user",
         )
         self.user.set_password("userpassword")
         self.user.save()
         self.token = Token.objects.create(user=self.user)
-        self.url = reverse("user.carbon-footprint")
-
+        self.url = reverse("user.data")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
         return super().setUp()
 
-    def test_carbon_footprint_success(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
+    def test_user_data_success(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertIn("carbon_footprint", response.json())
-        self.assertEqual(response.json()["carbon_footprint"], 0)
+        self.assertIn("name", response.json())
+        self.assertIn("last_name", response.json())
+        self.assertIn("birth_date", response.json())
+        self.assertIn("location", response.json())
 
-    def test_carbon_footprint_success_2(self):
-        self.user.carbon_footprint = 10
-        self.user.save()
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.token.key}")
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("carbon_footprint", response.json())
-        self.assertEqual(response.json()["carbon_footprint"], 10)
-
-    def test_carbon_footprint_unauthorized(self):
+    def test_user_data_unauthorized(self):
+        self.client.credentials()
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 401)
         self.assertIn("detail", response.json())
+        self.assertEqual(
+            response.json()["detail"], "Authentication credentials were not provided.")
+
+    def test_user_data_invalid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token invalidtoken")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("detail", response.json())
+        self.assertEqual(response.json()["detail"], "Invalid token.")
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 
-from .views import UserView
+import accounts.views as UserView
 
 urlpatterns = [
     path("user/register", UserView.register, name="user.register"),
     path("user/login", UserView.login, name="user.login"),
-    path(
-        "user/<int:pk>",
-        UserView.UserRetrieveUpdateDestroy.as_view(),
-        name="user.detail",
-    ),
+    path("user/data", UserView.UserData.as_view(), name="user.data"),
     path("user/all", UserView.UserList.as_view(), name="user.list"),
     path("admin/login", UserView.admin_login, name="admin.login"),
-    path("user/carbon-footprint", UserView.get_carbon_footprint,
-         name="user.carbon-footprint"),
 ]
