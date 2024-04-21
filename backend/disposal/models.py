4a7c0c5d267dcc889a2a96207bbf9c6c7b99a0a0
Sergio Córdoba <scordobamuriel@gmail.com>
2024-04-21 18:10:17 -0500

    Add image field
    Added the image field so the admin can upload images to the projects

--- backend/disposal/models.py
+++ backend/disposal/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import User
+from django.utils.translation import gettext_lazy as _
+
+def upload_to_projects(instance, filename):
+    return f"projects/{filename}".format(filename)
 
 
 def upload_to(instance, filename):
@@ -91,7 +95,7 @@ class Project(models.Model):
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    image = models.CharField(max_length=50)
+    image = models.ImageField(_("Image"), upload_to=upload_to_projects, default="projects/default.jpg")
     location = models.CharField(max_length=50)
     description = models.TextField()
     goal_tons = models.CharField(max_length=50)
