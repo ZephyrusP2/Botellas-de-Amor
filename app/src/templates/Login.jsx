39481f1517ca4776f5870aa8af777b598f4b7ee3
Foxnta <nicolastovar9@gmail.com>
2024-04-22 11:36:15 -0500

    Fix bugs in app
    solve bugs in different pages

--- app/src/templates/Login.jsx
+++ app/src/templates/Login.jsx
@@ -16,12 +16,21 @@ import UserService from "../services/user";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Login = ({ navigation }) => {
+
+  const [errorMessage, setErrorMessage] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
-  const [error, setError] = useState("");
+
 
   login = async (email, password) => {
+    if (!email || !password) {
+      setErrorMessage("Llena todos los campos");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+      return;
+    }
     const userData = {
       email: email,
       password: password,
@@ -34,12 +43,14 @@ const Login = ({ navigation }) => {
         AsyncStorage.setItem("email", userData.email);
         const id = response.data.id.toString();
         AsyncStorage.setItem("id", id);
-        setError("");
         navigation.navigate("Content");
       })
       .catch((error) => {
-        console.log("login", error);
-        setError(error.toString());
+        console.log("login", error.response.data);
+        setErrorMessage(error.response.data.error.toString());
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 5000);
       });
   };
 
@@ -47,7 +58,7 @@ const Login = ({ navigation }) => {
     navigation.navigate("Register");
   };
 
-  onForgotPassword = () => {};
+  onForgotPassword = () => { };
 
   return (
     <StyledBackground bg="secondary" style={styles.background}>
@@ -99,6 +110,11 @@ const Login = ({ navigation }) => {
               secureTextEntry={true}
             />
 
+            {errorMessage && (
+              <StyledText color="danger" size="medium" align="center">
+                {errorMessage}
+              </StyledText>
+            )}
             <StyledButton onPress={() => login(email, password)}>
               Comenzar
             </StyledButton>
@@ -119,6 +135,7 @@ const Login = ({ navigation }) => {
               <StyledText size="medium" color="primary" align="center">
                 Recuperar contraseña
               </StyledText>
+
             </TouchableOpacity>
           </View>
         </StyledBackground>
@@ -141,7 +158,7 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     width: "100%",
-    height: "55%",
+    height: "50%",
     justifyContent: "center",
     alignItems: "center",
   },
