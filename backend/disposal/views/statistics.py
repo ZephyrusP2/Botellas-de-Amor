5f38be2c1f3beb6671af0a0c8f9d747a19dcb812
Sergio Córdoba <scordobamuriel@gmail.com>
2024-04-23 23:29:48 -0500

    Add statistics
    Added statistics to the admin panel

--- backend/disposal/views/statistics.py
+++ backend/disposal/views/statistics.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework import generics, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import (api_view, authentication_classes, permission_classes)
+from rest_framework import status
 
 from accounts.models import User
 from backend.permissions import IsAdminOrOperator, IsAdminOrOperatorOrSelf
@@ -20,11 +21,9 @@ def total_bottles_contributed(request):
 
 
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated])
-@authentication_classes([TokenAuthentication])
 def plastic_footprint_reduced(request):
     total_users = User.objects.count()
-    plastic_footprint = User.objects.filter(user=request.user).aggregate(Sum('plastic_footprint'))['total_plastic_footprint'] or 0
+    plastic_footprint = User.objects.aggregate(total_plastic_footprint=Sum('plastic_footprint'))['total_plastic_footprint'] or 0
     plastic_footprint_average = plastic_footprint / total_users
     return JsonResponse({'plastic_footprint_average': plastic_footprint_average})
 
@@ -39,14 +38,20 @@ def users(request):
 @permission_classes([permissions.IsAuthenticated])
 @authentication_classes([TokenAuthentication])
 def most_contributed_bottles_by_gender(request):
-    gender = Disposition.objects.filter(gender=request.user.gender)
-    if gender == 'Femenino':
-        women = Disposition.objects.filter(gender=gender).aggregate(Sum('bottles'))['total_bottles_by_women'] or 0
-    elif gender == 'Masculino':
-        men = Disposition.objects.filter(gender=gender).aggregate(Sum('bottles'))['total_bottles_by_men'] or 0
-    
-    response = []
-    response.append({'women': women, 'men': men})
+    women = 0
+    men = 0
+
+    disposals = Disposition.objects.filter(user=request.user)
+    #total_bottles = disposals.aggregate(Sum('bottles'))['bottles__sum'] or 0
+    gender = request.user.gender
+
+    if gender:
+        if gender.lower() == 'femenino':  # Convert to lowercase for comparison
+            women = disposals.aggregate(total_bottles_by_women=Sum('bottles'))['total_bottles_by_women'] or 0
+        elif gender.lower() == 'masculino':  # Convert to lowercase for comparison
+            men = disposals.aggregate(total_bottles_by_men=Sum('bottles'))['total_bottles_by_men'] or 0
+
+    response = [{"women": women, "men": men}]
     return JsonResponse(response, safe=False)
 
 @api_view(['GET'])
@@ -65,7 +70,7 @@ def top_5_sites(request):
     top_5_sites = sorted_sites[:5]
     response = []
     for site, bottles in top_5_sites:
-        response.append({'site': site, 'bottles': bottles})
+        response.append({'site': site.name, 'bottles': bottles})
 
     return JsonResponse(response, safe=False)
 
@@ -85,15 +90,13 @@ def top_5_users(request):
     top_5_users = sorted_users[:5]
     response = []
     for user, bottles in top_5_users:
-        response.append({'user': user, 'bottles': bottles})
+        response.append({'user': user.get_full_name(), 'bottles': bottles})
 
     return JsonResponse(response, safe=False)
 
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated])
-@authentication_classes([TokenAuthentication])
 def total_kilos_contributed(request):
-    total_kilos = Disposition.objects.filter(user=request.user).aggregate(Sum('kilos'))['total_kilos_contributed'] or 0
+    total_kilos = Disposition.objects.aggregate(total_kilos_contributed=Sum('weight'))['total_kilos_contributed'] or 0
     return JsonResponse({'total_kilos': total_kilos}, safe=False)
 
 def get_age(birth_date):
@@ -105,7 +108,7 @@ def get_age(birth_date):
 @permission_classes([permissions.IsAuthenticated])
 @authentication_classes([TokenAuthentication])
 def average_age(request):
-    birth_dates = User.objects.filetr(birth_date=request.birth_date)
+    birth_dates = User.objects.filter(birth_date=request.user.birth_date)
     user_ages = [get_age(user.birth_date) for user in birth_dates]
 
     if user_ages:
@@ -119,16 +122,27 @@ def average_age(request):
 @permission_classes([permissions.IsAuthenticated])
 @authentication_classes([TokenAuthentication])
 def projected_bottles_contribution(request, days):
+    days = int(days)
     end_date = timezone.now()
     start_date = end_date - timedelta(days=days)
 
-    disposals = Disposition.objects.filter(user=request.user, timestamp__range=(start_date, end_date))
+    disposals = Disposition.objects.filter(user=request.user, created_at__range=(start_date, end_date))
 
-    x = np.array([[(disposal.timestamp - start_date).days] for disposal in disposals])
-    y = np.array([disposal.bottles for disposal in disposals])
+    if disposals.exists():
+        x = np.array([[(disposal.created_at - start_date).days] for disposal in disposals])  # Reshape to 2D array
+        y = np.array([disposal.bottles for disposal in disposals])  # No need to reshape y
 
-    model = LinearRegression().fit(x, y)
-    future_days = np.array([[days]])
-    projected_bottles = model.predict(future_days)[0]
+        # Reshape x to have shape (n_samples, 1)
+        x = x.reshape(-1, 1)
 
-    return JsonResponse({'projected_bottles': projected_bottles}, safe=False)
+        model = LinearRegression().fit(x, y)
+        future_days = np.array([[days]])
+
+        # Reshape future_days to have shape (1, 1)
+        future_days = future_days.reshape(1, -1)
+
+        projected_bottles = model.predict(future_days)[0]
+
+        return JsonResponse({'projected_bottles': projected_bottles}, safe=False)
+    else:
+        return JsonResponse({'error': 'No data available for the specified time range'}, status=status.HTTP_404_NOT_FOUND)
